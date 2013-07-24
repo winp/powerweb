0dac7baf75b7def01a8f01581f548cac039f3891
Subhajit Sahu <wolfram77@gmail.com>
2013-07-23 19:35:42 -0700

    hello world with cmd.exe from ASP.NET

---
+++ powerWeb/powerWeb/default.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Diagnostics;
+using System.IO;
+
+namespace powerWeb
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ProcessStartInfo psi = new ProcessStartInfo();
+            psi.FileName = @"C:\Windows\system32\cmd.exe";
+            psi.WorkingDirectory = Path.GetDirectoryName(psi.FileName);
+            Process.Start(psi);
+        }
+    }
+}
